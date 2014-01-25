d980dc667a44d0bab8068106d92695fa48b4eff4
Cyborg771 <cyborg771@gmail.com>
2014-01-25 12:11:00 -0800

    changes to projectile

--- src/sdp/ggj14/game/entities/Projectile.java
+++ src/sdp/ggj14/game/entities/Projectile.java
@@ -1,17 +1,32 @@
 package sdp.ggj14.game.entities;
 
 import org.dyn4j.dynamics.Body;
+import org.dyn4j.dynamics.Force;
+import org.dyn4j.geometry.Vector2;
 
 import sdp.ggj14.game.Level;
 import sdp.ggj14.util.Sprite;
 
 public class Projectile extends Unit {
-	public static final int PROJECTILE_SIZE = 8;
+	public static final int PROJECTILE_WIDTH = 24;
+	public static final int PROJECTILE_HEIGHT = 9;
 	
 	public Projectile(double x, double y, int hp) {
-		super(x, y, PROJECTILE_SIZE, PROJECTILE_SIZE, hp, 1);
+		super(x+10, y, PROJECTILE_WIDTH, PROJECTILE_HEIGHT, hp, 1);
 		super.sprite = new Sprite(new String[] {"/projectiles/player.png"}, 1);
 		super.setGravityScale(0);
+		
+		Force test = new Force(50, 0) {
+			@Override
+			public boolean isComplete(double elapsedTime){
+				return false;
+			}
+		};
+		this.applyForce(test);
+	}
+	
+	@Override
+	public void update(double elapsedTime) {
 	}
 	
 	@Override
