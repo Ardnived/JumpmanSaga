707718eb361ac336bc6acaecf50f23cbbf310e9a
Cyborg771 <cyborg771@gmail.com>
2014-01-25 10:05:41 -0800

    air bar based on player HP

--- src/sdp/ggj14/game/UserInterface.java
+++ src/sdp/ggj14/game/UserInterface.java
@@ -8,14 +8,17 @@ import sdp.ggj14.util.ImageLoader;
 public class UserInterface {
 	
 	public double air = 100;
+	private Level level;
 	
-	public UserInterface() {
-		// TODO Auto-generated constructor stub
+	public UserInterface(Level level) {
+		this.level = level;
 	}
 	
 	public void paint(Graphics graphics) {
 		graphics.setColor(Color.WHITE);
 		//graphics.drawString("Jumpman Saga v0.1", 15, 15);
+		
+		air = level.getPlayer().getHP();
 		
 		graphics.drawImage(ImageLoader.get("/ui/airbar.png"), 13, 13, (int)(117*(air/100)), 9, null);
 		graphics.drawImage(ImageLoader.get("/ui/airbarcontainer.png"), 10, 10, 123, 15, null);
