89a071c818b127db663e95d9ee71943281b6f457
Cyborg771 <cyborg771@gmail.com>
2014-01-25 09:59:43 -0800

    UI draws air bar

--- src/sdp/ggj14/game/UserInterface.java
+++ src/sdp/ggj14/game/UserInterface.java
@@ -3,15 +3,22 @@ package sdp.ggj14.game;
 import java.awt.Color;
 import java.awt.Graphics;
 
-public class UserInterface {
+import sdp.ggj14.util.ImageLoader;
 
+public class UserInterface {
+	
+	public double air = 100;
+	
 	public UserInterface() {
 		// TODO Auto-generated constructor stub
 	}
 	
 	public void paint(Graphics graphics) {
 		graphics.setColor(Color.WHITE);
-		graphics.drawString("Jumpman Saga v0.1", 15, 15);
+		//graphics.drawString("Jumpman Saga v0.1", 15, 15);
+		
+		graphics.drawImage(ImageLoader.get("/ui/airbar.png"), 13, 13, (int)(117*(air/100)), 9, null);
+		graphics.drawImage(ImageLoader.get("/ui/airbarcontainer.png"), 10, 10, 123, 15, null);
 	}
 
 }
