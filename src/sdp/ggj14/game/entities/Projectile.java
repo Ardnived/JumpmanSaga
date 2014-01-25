00242ddaca277662358e9201a8d14e2a5007d7c8
Devindra Payment <Arden.ived@gmail.com>
2014-01-25 10:23:30 -0800

    stuff

--- src/sdp/ggj14/game/entities/Projectile.java
+++ src/sdp/ggj14/game/entities/Projectile.java
@@ -9,7 +9,7 @@ public class Projectile extends Unit {
 	public static final int PROJECTILE_SIZE = 8;
 	
 	public Projectile(double x, double y, int hp) {
-		super(x, y, 8, 8, hp);
+		super(x, y, PROJECTILE_SIZE, PROJECTILE_SIZE, hp);
 		super.sprite = new Sprite(new String[] {"/projectiles/player_default.png"}, 1);
 		super.setGravityScale(0);
 	}
