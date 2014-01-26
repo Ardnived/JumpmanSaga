afb0263c354e6ca5c4ab7752fa43d1de5b3237b0
Devindra Payment <Arden.ived@gmail.com>
2014-01-25 16:23:46 -0800

    Added JSONLoader and an example level json

--- src/sdp/ggj14/game/entities/PowerUp.java
+++ src/sdp/ggj14/game/entities/PowerUp.java
@@ -1,18 +1,32 @@
 package sdp.ggj14.game.entities;
 
+import java.awt.image.BufferedImage;
+
 import org.dyn4j.dynamics.Body;
 
 import sdp.ggj14.game.Level;
+import sdp.ggj14.util.ImageLoader;
 import sdp.ggj14.util.Sprite;
 
 
 public class PowerUp extends Unit {
 	enum Type {
+		DIODE("artifact/s02.png"),
+		HELIX("artifact/s06.png"),
+		OXIDE("new_life_form/s08.png");
 		
-	}
+		String sprite;
+		
+		Type(String sprite) {
+			this.sprite = "/collectables/"+sprite;
+		}
+	};
 	
-	public PowerUp(double x, double y, int hp) {
-		super(x, y, 48, 48, hp);
+	Type type;
+	
+	public PowerUp(Type type, double x, double y) {
+		super(x, y, 48, 48, 1);
+		this.type = type;
 		
 		super.sprite = new Sprite(new String[] {
 				"/collectables/artifact_s02.png"}, 1);
@@ -25,6 +39,11 @@ public class PowerUp extends Unit {
 		}
 		
 		return false;
+	}
+	
+	@Override
+	public BufferedImage getSprite() {
+		return ImageLoader.get(type.sprite);
 	}
 
 }
